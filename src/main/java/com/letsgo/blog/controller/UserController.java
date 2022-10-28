d19061221bfa4dee661d893d45b183d8f3ada6d3
Vishal Kumar <vishal.kumar3@publicissapient.com>
2022-10-28 23:12:34 +0530

    completed user crud api

--- src/main/java/com/letsgo/blog/controller/UserController.java
+++ src/main/java/com/letsgo/blog/controller/UserController.java
@@ -5,10 +5,11 @@ import com.letsgo.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,6 +19,19 @@ public class UserController {
     private UserService userService;
 
     //GET  - get user
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUser() {
+        List<UserDto>  userDtoList = this.userService.getAllUser();
+        return new ResponseEntity<>(userDtoList, HttpStatus.OK);
+    }
+
+
+    //GET  - get user
+    @GetMapping("/{theUserId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable int theUserId) {
+        UserDto userDtoList = this.userService.getUserById(theUserId);
+        return new ResponseEntity<>(userDtoList, HttpStatus.OK);
+    }
 
     //POST - create user
     @PostMapping("/create-user")
@@ -26,11 +40,21 @@ public class UserController {
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
     }
 
-
-
     //PUT - update user
+    @PutMapping("/update-user/{theUserId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto theUserDto, @PathVariable int theUserId) {
+        UserDto updatedUserDto = this.userService.updateUser(theUserDto,theUserId);
+        return new ResponseEntity(updatedUserDto,HttpStatus.OK);
+    }
 
     //DELETE - delete user
+    @DeleteMapping("delete-user/{theUserId}")
+    public ResponseEntity<Map<String,String>> deleteUser(@RequestBody UserDto theUserDto, @PathVariable int theUserId) {
+        this.userService.deleteUser(theUserId);
+        Map<String,String> messageMap = new HashMap<>();
+        messageMap.put("message","User deleted successfully");
+        return new ResponseEntity(messageMap,HttpStatus.OK);
+    }
 
 
 }
