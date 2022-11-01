2c3ec5e91518752baafbd9b0b40a20fd7e5533b8
Vishal Kumar <vishal.kumar3@publicissapient.com>
2022-11-01 20:56:55 +0530

    Exception handling when user not found and return proper message

---
+++ src/main/java/com/letsgo/blog/dto/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.letsgo.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean success;
+
+}
