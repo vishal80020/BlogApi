bca6009d1168a4dd912e99a702a7dd68df625f1b
Vishal Kumar <vishal.kumar3@publicissapient.com>
2022-12-26 21:19:38 +0530

    Enable JWT in Swagger

--- src/main/java/com/letsgo/blog/config/SwaggerConfig.java
+++ src/main/java/com/letsgo/blog/config/SwaggerConfig.java
@@ -4,20 +4,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+
+    private ApiKey getApiKey() {
+        return new ApiKey("JWT",AUTHORIZATION_HEADER,"header");
+    }
+
+    private List<SecurityContext> getSecurityContexts() {
+        return Arrays.asList(SecurityContext.builder().securityReferences(getSecurityReferences()).build());
+    }
+
+    private List<SecurityReference> getSecurityReferences() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global","accessEverything");
+        return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[] {authorizationScope}));
+    }
+
 
     @Bean
     public Docket apiDoc() {
         return new Docket(DocumentationType.SWAGGER_2)
                     .apiInfo(getInfo())
+                    .securityContexts(getSecurityContexts())
+                    .securitySchemes(Arrays.asList(getApiKey()))
                     .select()
                     .apis(RequestHandlerSelectors.any())
                     .paths(PathSelectors.any()).build();
